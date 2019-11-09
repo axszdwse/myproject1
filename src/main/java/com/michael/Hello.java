fa5bcd02261f494e17b6b8812d8f747f246c53b9
Michael Wang <a0284659570@gmail.com>
2019-11-09 11:57:10 +0800

    Java and Kotlin variable, data type

--- src/main/java/com/michael/Hello.java
+++ src/main/java/com/michael/Hello.java
@@ -5,5 +5,15 @@ public class Hello {
         //System.out.println("Hello World");
         Person person=new Person();
         person.hello();
+        int age=19; //short 16bits;int 32bits;long 64bits;
+        Integer age2=19;//參造資料型態;包裝類別
+        char c='A';//16bits
+        Character c2='A';
+        byte b=120;//8bits
+        float weight=66.5f;//32bits後面要加f
+        double height=1.7;//64bits
+        boolean adult=true;
+        boolean enroll=false;
+        String name="Michael";
     }
 }
--- src/main/kotlin/com/michael/Hello.kt
+++ src/main/kotlin/com/michael/Hello.kt
@@ -5,6 +5,10 @@ fun main(args: Array<String>) {
     //Human().hello()
     val h=Human()
     h.hello()
+    val age=19 //變數型態的自動推斷 val不可變 var可變
+    var weight=66.5
+    var name:String
+    name = "Michael"
 }
 
 class Human{
